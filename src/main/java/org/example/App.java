38a952b4d3311c37c94ca7369d3ed45128865e67
Aditya <adityapatil.rajesh@qapitol.com>
2023-11-02 08:53:43 +0530

    Added 3 sout messages

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Gm tom !");
+        System.out.println( "Gm sam !");
+        System.out.println( "Gm martin !");
     }
 }
