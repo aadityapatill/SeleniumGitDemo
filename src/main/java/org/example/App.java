6a221f28119105c8ab2ea391376bd54de2b9fbe1
Aditya <adityapatil.rajesh@qapitol.com>
2023-11-02 12:02:53 +0530

    added statement

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,10 +8,12 @@ public class App
 {
     public static void main( String[] args )
     {
+
+        System.out.println( "Gm tom !");
         System.out.println( "Gm tom !");
         //added methods
         if (true){
-            System.out.println("Hi");
+            System.out.println("tom");
         }
 
     }
