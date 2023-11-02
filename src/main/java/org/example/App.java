65e97c82c006e029dd185ae16a90338c67f06427
Aditya <adityapatil.rajesh@qapitol.com>
2023-11-02 09:01:50 +0530

    Added 1 new sout messages (gm)

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,9 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Gm tom !");
-        System.out.println( "Gm ram!");
-        System.out.println( "Gm martin !");
-        System.out.println( "Good morning!");
-        System.out.println( "Good afternoon!");
+
     }
 }
